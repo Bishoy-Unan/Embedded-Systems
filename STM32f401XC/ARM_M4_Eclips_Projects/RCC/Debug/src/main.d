ca8fef1d89813047b49e4f974700a134e6d9ce48
Bishoy <bishoy.14younan@gmail.com>
2024-02-13 13:52:16 +0200

    RCC Eclips

---
+++ STM32f401XC/ARM_M4_Eclips_Projects/RCC/Debug/src/main.d
@@ -0,0 +1,8 @@
+src/main.d: ../src/main.c ../include/STD_TYPES.h \
+ ../include/MRCC_Interface.h ../include/STD_TYPES.h
+
+../include/STD_TYPES.h:
+
+../include/MRCC_Interface.h:
+
+../include/STD_TYPES.h:
---
+++ STM32f401XC/ARM_M4_Eclips_Projects/RCC/Debug/src/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/MRCC_Program.c \
+../src/main.c 
+
+OBJS += \
+./src/MRCC_Program.o \
+./src/main.o 
+
+C_DEPS += \
+./src/MRCC_Program.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F401xC -DUSE_HAL_DRIVER -DHSE_VALUE=25000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wa,-adhlns="$@.lst" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/main.o: ../src/main.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=soft -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F401xC -DUSE_HAL_DRIVER -DHSE_VALUE=25000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wa,-adhlns="$@.lst" -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/main.d" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
